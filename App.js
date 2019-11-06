0799d7b82b5590abb60b2785639519020829b61b
Diego Avila <diegob.avila@hotmail.com>
2019-11-05 22:41:01 -0300

    Consegui padronizar a lista de cards

--- App.js
+++ App.js
@@ -1,5 +1,28 @@
-import Home from './src/pages/Home'
-export default Home;
+import React from 'react';
+import { ScrollView ,StyleSheet, Text, View } from 'react-native';
 
 
 
+import List from './src/components/List'
+
+export default class App extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <View>
+              <List/>
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    
+
+
+  });
---
+++ src/components/List.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import { ScrollView, StyleSheet,Text, View } from 'react-native';
+
+import { Card } from 'react-native-elements';
+
+const List = props => {
+        
+    return (
+        <ScrollView >
+              <View style={styles.list}>
+                <View style={styles.container}>
+                  <Card title="Titulo1" style={styles.card}>
+                    <Text style={styles.item}>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</Text>
+                  </Card>
+                </View>
+                <View style={styles.container}>
+                  <Card title="Titulo1" style={styles.card}>
+                    
+                  </Card>
+                </View>
+                <View style={styles.container}>
+                  <Card title="Titulo1" style={styles.card}>
+                    
+                  </Card>
+                </View>
+                <View style={styles.container}>
+                  <Card title="Titulo1" style={styles.card}>
+                    
+                  </Card>
+                </View>
+                <View style={styles.container}>
+                  <Card title="Titulo1" style={styles.card}>
+                    
+                  </Card>
+                </View>
+                <View style={styles.container}>
+                  <Card title="Titulo1" style={styles.card}>
+                    
+                  </Card>
+                </View>
+                <View style={styles.container}>
+                  <Card title="Titulo1" style={styles.card}>
+                    
+                  </Card>
+                </View>
+                <View style={styles.container}>
+                  <Card title="Titulo1" style={styles.card}>
+                    
+                  </Card>
+                </View>
+                <View style={styles.container}>
+                  <Card title="Titulo1" style={styles.card}>
+                    
+                  </Card>
+                </View>
+                <View style={styles.container}>
+                  <Card title="Titulo1" style={styles.card}>
+                    
+                  </Card>
+                </View><View style={styles.container}>
+                  <Card title="Titulo1" style={styles.card}>
+                    
+                  </Card>
+                </View>
+                <View style={styles.container}>
+                  <Card title="Titulo1" style={styles.card}>
+                    
+                  </Card>
+                </View>
+                <View style={styles.container}>
+                  <Card title="Titulo1" style={styles.card}>
+                    
+                  </Card>
+                </View>
+                <View style={styles.container}>
+                  <Card title="Titulo1" style={styles.card}>
+                    
+                  </Card>
+                </View>
+                <View style={styles.container}>
+                  <Card title="Titulo1" style={styles.card}>
+                    
+                  </Card>
+                </View>
+                <View style={styles.container}>
+                  <Card title="Titulo1" style={styles.card}>
+                    
+                  </Card>
+                </View>
+                <View style={styles.container}>
+                  <Card title="Titulo1" style={styles.card}>
+                    
+                  </Card>
+                </View>
+                <View style={styles.container}>
+                  <Card title="Titulo1" style={styles.card}>
+                    
+                  </Card>
+                </View>
+                <View style={styles.container}>
+                  <Card title="Titulo1" style={styles.card}>
+                    
+                  </Card>
+                </View>
+                <View style={styles.container}>
+                  <Card title="Titulo1" style={styles.card}>
+                    
+                  </Card>
+                </View>
+              </View>
+              
+            </ScrollView>
+    )
+};
+const styles = StyleSheet.create({
+    list: {
+        display: 'flex',
+        margin:10,
+        flexWrap: 'wrap',
+        flexDirection: 'row',
+        alignContent: 'center',
+        marginTop: 20,
+        
+      },
+      container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'column',
+        alignContent: 'space-between',
+        width: '50%',
+        
+      },
+      card: {
+        width: '50%',
+      },
+      item: {
+        height: 1
+  
+      }
+    
+});
+export default List;
